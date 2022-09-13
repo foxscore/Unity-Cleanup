17abd30edaa16f3cdae38d24771ae6d63629c4d4
Felix Kaiser <felixkaiser2000@protonmail.com>
2022-09-13 22:52:49 +0200

    Initial Commit

---
+++ uc/Options.cs
@@ -0,0 +1,26 @@
+﻿using CommandLine;
+
+namespace uc;
+
+public class Options
+{
+    // Path
+    [Option('p', "path", Required = false, HelpText = "Path of the directory to be processed.")]
+    public string Path { get; set; } = Environment.CurrentDirectory;
+    
+    // Recursive
+    [Option('r', "recursive", Required = false, HelpText = "Scan recursively.")]
+    public bool Recursive { get; set; } = false;
+    
+    // Keep Logs
+    [Option('l', "keep-logs", Required = false, HelpText = "Keep the Logs directory.")]
+    public bool KeepLogs { get; set; } = false;
+    
+    // Keep obj
+    [Option('o', "keep-obj", Required = false, HelpText = "Keep the obj directory.")]
+    public bool KeepObj { get; set; } = false;
+    
+    // Keep Temp
+    [Option('t', "keep-temp", Required = false, HelpText = "Keep the Temp directory.")]
+    public bool KeepTemp { get; set; } = false;
+}
---
+++ uc/Program.cs
@@ -0,0 +1,75 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CommandLine;
+using uc;
+
+var arguments = Parser.Default.ParseArguments<Options>(args);
+
+if (arguments.Errors.Any() || arguments.Tag == ParserResultType.NotParsed)
+    Environment.Exit(1);
+
+if (!Directory.Exists(arguments.Value.Path))
+{
+    Console.WriteLine($"Directory not found: {arguments.Value.Path}");
+    Environment.Exit(1);
+}
+
+ScanDirectory(arguments.Value.Path, arguments.Value.Recursive);
+
+// This tool checks if a directory is a unity project by checking if it contains the "Assets", "Packages", and "ProjectSettings" directories.
+// If it is, it will delete the "Library", "Logs", "obj", "Temp" directories.
+// Otherwise, it will go through the subdirectories and repeat the process, IF the recursive option is set.
+
+void ScanDirectory(string directory, bool recursive)
+{
+    // Check if this is a unity project and runs the cleanup if it is
+    
+    var assets = Path.Combine(directory, "Assets");
+    var packages = Path.Combine(directory, "Packages");
+    var projectSettings = Path.Combine(directory, "ProjectSettings");
+    
+    if (Directory.Exists(assets) && Directory.Exists(packages) && Directory.Exists(projectSettings))
+        CleanUp(directory);
+    else
+        // This is not a unity project, go through the subdirectories
+        if (recursive)
+            foreach (var subdirectory in Directory.GetDirectories(directory))
+                ScanDirectory(subdirectory, true);
+}
+
+void CleanUp(string directory)
+{
+    // Delete the "Library", "Logs", "obj", "Temp" directories
+    var dirName = Path.GetFileName(directory);
+    
+    var library = Path.Combine(directory, "Library");
+    var logs = Path.Combine(directory, "Logs");
+    var obj = Path.Combine(directory, "obj");
+    var temp = Path.Combine(directory, "Temp");
+    
+    var hasLibrary = Directory.Exists(library);
+    var hasLogs = !arguments.Value.KeepLogs && Directory.Exists(logs);
+    var hasObj = !arguments.Value.KeepObj && Directory.Exists(obj);
+    var hasTemp = !arguments.Value.KeepTemp && Directory.Exists(temp);
+    
+    if (!hasLibrary && !hasLogs && !hasObj && !hasTemp) return;
+    
+    Console.Write($"Cleaning up {dirName}");
+    
+    if (Directory.Exists(library))
+        Directory.Delete(library, true);
+    
+    if (!arguments.Value.KeepLogs && Directory.Exists(logs))
+        Directory.Delete(logs, true);
+    
+    if (!arguments.Value.KeepObj && Directory.Exists(obj))
+        Directory.Delete(obj, true);
+    
+    if (!arguments.Value.KeepTemp && Directory.Exists(temp))
+        Directory.Delete(temp, true);
+    
+    // Clear the line
+    Console.SetCursorPosition(0, Console.CursorTop);
+    
+    Console.WriteLine($"Cleaned up {dirName} ");
+}
